0c108f4c3387f1ee3404a287849d8acad94c3b4d
Fernando Gutierrez <gutierrezhernandez.f86@gmail.com>
2024-03-21 13:13:38 -0600

    Build 1.0

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -6,7 +6,7 @@ interface CardProps {
 
 const Card = ({ accion }: CardProps) => {
     return (
-        <div className="relative mt-6 flex flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md w-52">
+        <div className="relative mt-3 flex flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md w-48">
             <div className="p-1">
                 <p className="block font-sans text-base font-light leading-relaxed text-inherit antialiased align-bottom text-center ">
                     {accion}
